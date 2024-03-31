722aee394fe65e4d9f0859900fd4e1bda1e9db55
yellowhatpro <yellowhatpro3119@gmail.com>
2024-03-31 14:30:28 +0530

    connectivity observer

--- app/src/main/java/dev/yellowhatpro/studyadda/common/hilt/NetworkModule.kt
+++ app/src/main/java/dev/yellowhatpro/studyadda/common/hilt/NetworkModule.kt
@@ -1,20 +1,31 @@
 package dev.yellowhatpro.studyadda.common.hilt
 
+import android.content.Context
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import dev.yellowhatpro.studyadda.data.remote.NetworkService
+import dev.yellowhatpro.studyadda.internet.ConnectivityObserver
+import dev.yellowhatpro.studyadda.internet.NetworkConnectivityObserver
 import retrofit2.Retrofit
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-object CoreModule {
+object NetworkModule {
     @Provides
     @Singleton
     fun providesAPIService() = Retrofit.Builder()
         .baseUrl("https://jsonplaceholder.typicode.com/")
         .build()
         .create(NetworkService::class.java)
+
+    @Provides
+    @Singleton
+    fun providesConnectivityObserver(
+        @ApplicationContext context : Context
+    ) : ConnectivityObserver = NetworkConnectivityObserver(context)
+
 }
---
+++ app/src/main/java/dev/yellowhatpro/studyadda/internet/ConnectivityObserver.kt
@@ -0,0 +1,13 @@
+package dev.yellowhatpro.studyadda.internet
+
+import kotlinx.coroutines.flow.Flow
+
+interface ConnectivityObserver {
+    fun observe(): Flow<Status>
+    enum class Status {
+        Available,
+        Unavailable,
+        Losing,
+        Lost
+    }
+}
